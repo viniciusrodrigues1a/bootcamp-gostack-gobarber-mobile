adc62f44f0fa3f9fbbeef3ca873801b6b11d954f
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-04-06 19:01:39 -0300

    :recycle: tornando App um componente para poder pegar informação do estado

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 
-import Routes from '~/routes';
+import { store, persistor } from '~/store';
+import App from './App';
 
-export default function src() {
+export default function Index() {
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <Routes />
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+          <App />
+        </PersistGate>
+      </Provider>
     </>
   );
 }
